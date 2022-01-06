bc5a86241004afcbe46094aa7ebd3fb59fe644e1
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-01-06 20:42:08 +0530

    Add. Four balls moving at different speed from left to right

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -4,7 +4,14 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH = 400;
     public static final int HEIGHT = 200;
     public static final int DIAMETER = 20;
-    public int x = 0;
+    public int x1 = 0;
+    public int x2 = 0;
+    public int x3 = 0;
+    public int x4 = 0;
+    public static final int SPEED_1 = 1;
+    public static final int SPEED_2 = 2;
+    public static final int SPEED_3 = 3;
+    public static final int SPEED_4 = 4;
 
 
     public static void main(String[] args) {
@@ -23,7 +30,10 @@ public class TryProcessing extends PApplet {
 
     @Override
     public void draw() {
-        ellipse(x, HEIGHT/2, DIAMETER, DIAMETER);
-        x++;
+        ellipse(x1 += SPEED_1, HEIGHT * 1 / 4, DIAMETER, DIAMETER);
+        ellipse(x2 += SPEED_2, HEIGHT * 2 / 4, DIAMETER, DIAMETER);
+        ellipse(x3 += SPEED_3, HEIGHT * 3 / 4, DIAMETER, DIAMETER);
+        ellipse(x4 += SPEED_4, HEIGHT * 4 / 4, DIAMETER, DIAMETER);
+
     }
 }
