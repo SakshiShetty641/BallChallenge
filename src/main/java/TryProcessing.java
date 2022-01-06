072024f4e315db745077122c1e1c0a39f0be7f72
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-01-06 19:28:22 +0530

    Add. To implement a window of given width and height

---
+++ src/main/java/TryProcessing.java
@@ -0,0 +1,13 @@
+import processing.core.PApplet;
+
+public class TryProcessing extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("TryProcessing", args);
+    }
+
+    @Override
+    public void settings() {
+        size(400, 200);
+    }
+
+}
